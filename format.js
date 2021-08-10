6e1d1700574dbb1cd5d65b6cfbc656abe24dbc9b
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-08-10 04:12:10 +0000

    formated first depth

--- format.js
+++ format.js
@@ -1,3 +1,29 @@
-const someObject = require("./countries.json");
+const Countries = require("./countries.json");
+const data = Countries.data;
 
+let newData = [];
 
+data.map(({ name, continent }, i) => {
+  if (i === 0) {
+    let firstNode = continent;
+    firstNode.countries = [name];
+    newData.push(firstNode);
+  } else if (newData.some((x) => continent.name === x.name)) {
+    newData.map((cont) => {
+      if (cont.name === continent.name) {
+        cont.countries.push(name);
+      }
+    });
+  } else {
+    let nextNode = continent;
+    nextNode.countries = [name];
+    newData.push(nextNode);
+  }
+});
+
+newData = newData.map((node)=>{
+  let {countries} =node
+  countries = countries.sort()
+  return node;
+})
+console.log(newData);
